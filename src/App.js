d82791b3d36a2473bd443dd8a6eb35c6a1ad16f4
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-13 17:06:41 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div> Hello </div>
+      <div> Hello my </div>
     ) 
   }
 }
