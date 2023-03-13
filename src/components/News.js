63954516757cd0ffba07dfabbacca0cc936fe996
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-13 22:02:50 +0530

    modify newsitem

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,7 @@ export class News extends Component {
       <div className='container my-3'>
         <h2> News App-Top Headlines</h2>
             <div className="col-md-4">
-                <NewsItem title="my title" description="my description"></NewsItem>
+                <NewsItem title="my title" description="my description" imageUrl="" ></NewsItem>
             </div>
             <div className="col-md-4">
                 <NewsItem title="my title" description="my description"></NewsItem>
