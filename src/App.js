024b0e1a87dc857f7b8da1920ef3158e5da8f232
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-15 20:44:26 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
-
+import { Route } from 'react-router-dom';
 import React, { Component } from 'react'
+import { Router } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div> 
+      <Router>
       <Navbar></Navbar>
-      <News pageSize={5} country="in" category="science"></News>
+      <Switch>
+        <Route  path="/">
+            <News pageSize={5} country="in" category="general"></News>
+        </Route>
+        <Route  path="/Business">
+            <News pageSize={5} country="in" category="business"></News>
+        </Route>
+        <Route  path="/Entertainment">
+            <News pageSize={5} country="in" category="entertainment"></News>
+        </Route>
+        <Route  path="/science">
+            <News pageSize={5} country="in" category="science"></News>
+        </Route>
+        <Route  path="/Health">
+            <News pageSize={5} country="in" category="health"></News>
+        </Route>
+      </Switch>   
+      </Router>
+      
       </div>
     ) 
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 export class Navbar extends Component {
   static propTypes = {}
 
@@ -8,29 +8,26 @@ export class Navbar extends Component {
       <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">News App</a>
+                <Link className= "navbar-brand" to="/">News App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link " aria-current="page" href="/">Home</a>
+                    <Link className= "nav-link " aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/">About</a>
+                    <Link className= "nav-link" to="/Business">Business</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/">Business</a>
+                    <Link className= "nav-link" to="/Entertainment">Entertainment</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/">Entertainment</a>
+                    <Link className= "nav-link" to="/General">General</Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/">General</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/">Health</a>
+                    <Link className= "nav-link" to="/Health">Health</Link>
                     </li>
                 </ul>
                
